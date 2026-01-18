dee7caaae57786c62122cbc01877960fcd695d9e
Dino-16 <ceejaycutie11@gmail.com>
2026-01-18 23:28:46 +0800

    11

--- app/Http/Controllers/Api/RequisitionsController.php
+++ app/Http/Controllers/Api/RequisitionsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Recruitment_Management\Requisition;
+use App\Models\Recruitment\Requisition;
 
 class RequisitionsController extends Controller
 {
