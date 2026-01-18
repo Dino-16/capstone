36356c778082b04c07ac81fa45ea636f78617ae0
Dino-16 <ceejaycutie11@gmail.com>
2026-01-18 23:24:16 +0800

    10

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GiveRewardsController;
+use App\Http\Controllers\Api\RequisitionsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/requisitions', [RequisitionsController::class, 'store']);
+
 
 Route::get('/give-rewards', [GiveRewardsController::class, 'index']);
 
